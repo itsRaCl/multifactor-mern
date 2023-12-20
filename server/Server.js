9ed9ee55952a7c16541c8d235370b38efa9e4a37
itsRaCl <rajclerk.2004@gmail.com>
2023-12-20 17:18:08 +0530

    added changes

---
+++ server/Server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const randomize = require("randomatic");
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Connect to MongoDB with connection pooling
+mongoose.connect("mongodb://127.0.0.1:27017/users", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // poolSize: 10,
+});
+
+const User = mongoose.model("User", {
+  email: String,
+  password: String,
+  otp: String,
+});
+
+// Function to send to the user's email
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+  if (user) {
+    console.log(user);
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    await User.findOneAndUpdate({ email }, { otp }, { upsert: true });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "turing.x.nerd@gmail.com",
+        pass: "nomx zaoq ybpo ntpl",
+      },
+    });
+
+    const mailOptions = {
+      from: "turing.x.nerd@gmail.com",
+      to: email,
+      subject: "OTP Verification",
+      text: `Your OTP is: ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(400).send(error.toString());
+      }
+      res.status(200).send("User Valid, OTP sent successfully.");
+    });
+  } else {
+    res.status(400).send("Invalid Credentials");
+  }
+});
+app.post("/verify-otp", async (req, res) => {
+  const { email, otp } = req.body;
+
+  // Check if the provided OTP is valid
+  const user = await User.findOne({ email, otp });
+  if (user) {
+    // Clear the OTP in the database after successful verification
+    await User.updateOne({ email }, { $unset: { otp: 1 } });
+    res.status(200).send("OTP verified successfully.");
+  } else {
+    res.status(400).send("Invalid OTP.");
+  }
+});
+
+/*
+ *app.post("/request-otp", async (req, res) => {
+ *  const { email } = req.body;
+ *
+ *  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+ *
+ *  await User.findOneAndUpdate({ email }, { otp }, { upsert: true });
+ *
+ *  const transporter = nodemailer.createTransport({
+ *    service: "gmail",
+ *    auth: {
+ *      user: "vrajclerk44@gmail.com",
+ *      pass: "vrajclerk44@",
+ *    },
+ *  });
+ *
+ *  const mailOptions = {
+ *    from: "vrajclerk04@gmail.com",
+ *    to: email,
+ *    subject: "OTP Verification",
+ *    text: `Your OTP is: ${otp}`,
+ *  };
+ *
+ *  transporter.sendMail(mailOptions, (error, info) => {
+ *    if (error) {
+ *      return res.status(500).send(error.toString());
+ *    }
+ *    res.status(200).send("OTP sent successfully.");
+ *  });
+ *});
+ */
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
